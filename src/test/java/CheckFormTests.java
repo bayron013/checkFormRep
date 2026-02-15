84e49f85ee982c21f5ac165cf75cf2c8b4afbff2
Simanin Vladislav <bayron013@gmail.com>
2026-02-15 22:43:28 +0300

    Сделал позитивный тест на проверку формы, пока без календаря

--- src/test/java/CheckFormTests.java
+++ src/test/java/CheckFormTests.java
@@ -6,6 +6,8 @@ import java.io.File;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 
@@ -23,16 +25,23 @@ public class CheckFormTests {
     @Test
     void CheckFormTest() {
 
+        // Открытие сайта
         open("");
         $$(".card-body").findBy(text("Forms")).click();
         $$(".router-link").findBy(text("Practice Form")).click();
 
+
+        // Ввод данных
         $("#firstName").setValue("Vladick");
         $("#lastName").setValue("Simba");
         $("#userEmail").setValue("winwin@maaal.zet");
         $("#gender-radio-2").click();
         $("#userNumber").setValue("79554433222");
-        $("#dateOfBirthInput").click();
+
+
+
+        $("#subjectsInput").setValue("History").pressEnter();
+        $("#subjectsInput").setValue("Maths").pressEnter();
         $("#hobbies-checkbox-1").click();
         $("#hobbies-checkbox-3").click();
         File imageFile = new File("src/test/resources/Ошибка в браузере.png");
@@ -44,17 +53,39 @@ public class CheckFormTests {
         $$("[role='option']").findBy(text("Agra")).shouldBe(visible).click();
         $("#submit").click();
 
-        $("text=Student Name").shouldHave(text("Vladick Simba"));
+
+        // Проверки формы
+        $(".modal-header").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").$(byText("Student Name")).parent().shouldHave(text("Vladick Simba"));
+        $(".table-responsive").$(byText("Student Email")).parent().shouldHave(text("winwin@maaal.zet"));
+        $(".table-responsive").$(byText("Gender")).parent().shouldHave(text("Female"));
+        $(".table-responsive").$(byText("Mobile")).parent().shouldHave(text("7955443322"));
+        $(".table-responsive").$(byText("Date of Birth")).parent().shouldHave(text("15 February,2026"));
+        $(".table-responsive").$(byText("Subjects")).parent().shouldHave(text("History, Maths"));
+        $(".table-responsive").$(byText("Hobbies")).parent().shouldHave(text("Sports, Music"));
+        $(".table-responsive").$(byText("Picture")).parent().shouldHave(text("Ошибка в браузере.png"));
+        $(".table-responsive").$(byText("Address")).parent().shouldHave(text("Приморское шоссе 15, к2"));
+        $(".table-responsive").$(byText("State and City")).parent().shouldHave(text("Uttar Pradesh Agra"));
 
 
 
 
-// Попытки настроить календарь
+//      Попытки использовать календарь
+//      1)
 //        $("react-datepicker__year-select select").click();
 //        $$(".react-datepicker__year-option").findBy(text("1989")).click();
 //        $(".react-datepicker__month-select").selectOption(month);
 //        $(".react-datepicker__year-select").selectOption(year);
 //        $$(".react-datepicker__day").findBy(text(day)).click();
+
+//      2)
+//        $("#dateOfBirthInput").click();
+//        $(".react-datepicker__year-select").click();
+//        $("option[value='1989']").click();
+//        $(".react-datepicker__month-select").click();
+//        $("option[value='9']").click();
+//        $(".react-datepicker__week").
+//                $(".react-datepicker__day react-datepicker__day--006").click();
 
 
 
